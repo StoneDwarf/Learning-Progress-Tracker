4cd7b1f82eb10f095454867a374ad1da868bf87c
StoneDwarf <131169770+StoneDwarf@users.noreply.github.com>
2024-06-01 12:09:11 +0400

    Update Progress.py

--- Progress.py
+++ Progress.py
@@ -16,11 +16,14 @@ def name_check(name, the_list, students_list, students_data):
     email = test_input[-1]
     if not first_name_pattern.match(first_name):
         print("Incorrect first name")
+        return
     elif not last_name_pattern.match(last_name):
         print("Incorrect last name")
+        return
     elif not email_pattern.match(email):
         print("Incorrect email")
-    for id_number, student in students_data.items()
+        return
+    for id_number, student in students_data.items():
         if email in student[0]:
             print('This email is already taken.')
             break
@@ -29,7 +32,7 @@ def name_check(name, the_list, students_list, students_data):
         new_id = students_list[-1] + 1
         students_list.append(new_id)
         students_data[new_id] = [[first_name + last_name, email], [0,0,0,0]]
-        print("The student has been added.")        
+        print("The student has been added.")
     return the_list, students_list, students_data
 
 
@@ -42,33 +45,61 @@ def student_add(students_list, students_data):
             print(f'Total {len(new_list)} students have been added.')
             return
         else:
-            name_check(user_input, new_list, students_list, students_data)   
+            name_check(user_input, new_list, students_list, students_data)
 
 
-def add_points():   # <<< finish this function
+def add_points(students_list, students_data):
     print("Enter an id and points or 'back' to return")
-    print(f'No student is found for id={}')
-    print('Incorrect points format')
-    id_input = name.split()
-    for index, point in enumerate.students_data[student_id][1]
-    print('Points updated')
+    while True:
+        student_id = input()
+        if student_id == 'back':
+            return students_list, students_data
+        else:
+            id_input = student_id.split()
+            if len(id_input) != 5:
+                print('Incorrect points format')
+                continue
+            try:
+                new_id = int(id_input[0])   # <<<<CORRECT!
+                points = list(map(int, id_input[1:]))
+                if new_id not in students_list:
+                    print(f'No student is found for id={new_id}')
+                elif any(point < 0 for point in points):
+                    print('Incorrect points format')
+                else:
+                    for index, point in enumerate.students_data[new_id][1]:
+                        students_data[new_id][1][index] += points[index]
+                    print('Points updated')
+  #  students_data = {new_id: [[first_name + last_name, email], [0,0,0,0]]}
+            except ValueError:
+                print('Incorrect points format')
+            except KeyError:
+                print(f'No student is found for id={id_input[0]}')
 
 
-def find_student():    # <<< finish this function
+def find_student(students_data):   #  students_data = {new_id: [[first_name + last_name, email], [0,0,0,0]]}
     print("Enter an id or 'back' to return")
-    print(f'id points: Python={}; DSA={}; Databases={}; Flask={}')
+    while True:
+        student_id = input()
+        if student_id == 'back':
+            return
+        try:
+            id = int(student_id)
+            print(f'id points: Python={students_data[id][1][0]}; DSA={students_data[id][1][1]}; Databases={students_data[id][1][2]}; Flask={students_data[id][1][3]}')
+        except:
+            print(f'No student is found for id={student_id}')
 
 
 def list_print(students):
-    if len(students) > 1:
+    if len(students) == 1:
         print('No students found')
     else:
         for item in students[1:]:
-            print(item)
+            print(f'Students:{item}')
 
 
 students_data = {}
-students_list = [0001]
+students_list = [0]
 none_commands = ['\t', None]
 while True:
     user_input = input()
@@ -83,5 +114,9 @@ while True:
         print("Enter 'exit' to exit the program")
     elif user_input == 'list':
         list_print(students_list)
+    elif user_input == 'add points':
+        add_points(students_list, students_data)
+    elif user_input == 'find':
+        find_student(students_data)
     else:
         print('unknown command!')
